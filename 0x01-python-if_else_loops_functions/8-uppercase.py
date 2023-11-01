b55bc841bcd9025c07d82200540a966cf7aa5a23
ufedop <ufedop@gmail.com>
2023-11-01 16:42:56 -0700

    updated

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def to_upper(character):
+def to_uper(character):
     if ord(character) >= 97 and ord(character) <= 122:
         return (ord(character) - 32)
     else:
@@ -8,5 +8,5 @@ def to_upper(character):
 def uppercase(string):
     string_new = ""
     for character in string:
-        string_new += "%c" % to_upper(char)
+        string_new += "%c" % to_uper(character)
     print("{:s}".format(string_new))
