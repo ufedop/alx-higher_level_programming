f6ac1bc9b363e2dfa8380bbf34c040b6f3a6cdbf
ufedop <ufedop@gmail.com>
2023-11-22 19:36:46 -0800

    python task classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Define square."""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length the side of a square.
+        """
+        self.__size = size
