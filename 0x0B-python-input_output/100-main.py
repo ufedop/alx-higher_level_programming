bbc2f6cbe62a307d794a30767eae1ce8ffb4df99
Daniel Ufedojo <ufedop@gmail.com>
2023-12-15 13:25:06 -0800

    solved tasks

---
+++ 0x0B-python-input_output/100-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+append_after = __import__('100-append_after').append_after
+
+append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
+
+At Holberton School,
+Python is really important,
+But it can be very hard if:
+- You don't get all Pythonic tricks
+- You don't have strong C knowledge.
---
+++ 0x0B-python-input_output/7-main.py
@@ -0,0 +1,1 @@
+add_item.json: No such file or directory
